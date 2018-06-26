5697fa2fc415e4b64e4f53e113fc814dfc3892a2
Alessandro Genova <alessandro.genova@kitware.com>
2018-06-26 17:15:38 -0400

    Clang format

--- tomviz/MainWindow.cxx
+++ tomviz/MainWindow.cxx
@@ -593,7 +593,7 @@ void MainWindow::operatorResultChanged(OperatorResult* res)
 {
   if (res) {
     m_ui->propertiesPanelStackedWidget->setCurrentWidget(
-    m_ui->operatorResultPropertiesScrollArea);
+      m_ui->operatorResultPropertiesScrollArea);
   }
 }
 
